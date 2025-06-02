c18e079b0ff2dc1bf7dd1e56d2f089d38c645a28
casparejensen <casparejensen@gmail.com>
2025-06-02 16:55:04 +0200

    Changes by paetur

--- test/SymbolTableTests.cpp
+++ test/SymbolTableTests.cpp
@@ -2,11 +2,10 @@
 // Created by wired on 5/29/25.
 //
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-//#include <doctest/doctest.h>
+#include <doctest/doctest.h>
 
-/*
+
 TEST_CASE("simple test") {
     auto x = 1;
     CHECK(x == 1);
 }
-*/
