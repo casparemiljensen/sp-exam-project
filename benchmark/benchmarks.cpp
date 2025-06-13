8c516d8c64b31347924311092e82acf9bc70b143
ilum1 <thomas.ilum.andersen@gmail.com>
2025-06-13 19:30:16 +0200

    Updated benchmarks

--- benchmark/benchmarks.cpp
+++ benchmark/benchmarks.cpp
@@ -17,13 +17,13 @@ static void seihr_single_core(benchmark::State &state) {
 
     debug_print("\nStarting single core seihr");
     for (const auto _: state) {
-        Examples::get_peak_average_serial(1500, vessel, 100, "H");
+        Examples::get_peak_average_serial(100, vessel, 100, "H");
     }
     debug_print("End single core seihr");
 }
 
 
-BENCHMARK(seihr_single_core)->Unit(benchmark::kMillisecond);
+BENCHMARK(seihr_single_core)->Unit(benchmark::kMillisecond)->Iterations(50);;
 
 
 static void seihr_multi_core(benchmark::State &state) {
@@ -31,39 +31,39 @@ static void seihr_multi_core(benchmark::State &state) {
 
     debug_print("\nStarting multi-core seihr");
     for (const auto _: state) {
-        Examples::get_peak_average(1500, vessel, 100, "H");
+        Examples::get_peak_average(100, vessel, 100, "H");
     }
     debug_print("End multi-core seihr");
 }
 
-BENCHMARK(seihr_multi_core)->Unit(benchmark::kMillisecond);
+BENCHMARK(seihr_multi_core)->Unit(benchmark::kMillisecond)->Iterations(50);
 
 
-static void circadian_rhythm_single_core_10_runs(benchmark::State &state) {
+static void circadian_rhythm_single_core_100_runs(benchmark::State &state) {
     auto vessel = Examples::circadian_rhythm();
 
-    debug_print("\nStarting circadian_rhythm_single_core_10_runs");
+    debug_print("\nStarting circadian_rhythm_single_core_100_runs");
     for (const auto _: state) {
-        Examples::get_peak_average_serial(1500, vessel, 10, "DA");
+        Examples::get_peak_average_serial(48, vessel, 100, "DA");
         // This finds peak average of H - H does not exist in all examples
     }
-    debug_print("End circadian_rhythm_single_core_10_runs");
+    debug_print("End circadian_rhythm_single_core_100_runs");
 }
 
-BENCHMARK(circadian_rhythm_single_core_10_runs)->Unit(benchmark::kMillisecond);
+BENCHMARK(circadian_rhythm_single_core_100_runs)->Unit(benchmark::kMillisecond)->Iterations(50);
 
-static void circadian_rhythm_single_core_10_runs_optimized(benchmark::State &state) {
+static void circadian_rhythm_single_core_100_runs_optimized(benchmark::State &state) {
     auto vessel = Examples::circadian_rhythm();
 
-    debug_print("\nStarting circadian_rhythm_single_core_10_runs_optimized");
+    debug_print("\nStarting circadian_rhythm_single_core_100_runs_optimized");
     for (const auto _: state) {
-        Examples::get_peak_average_serial_optimized(1500, vessel, 10, "DA");
+        Examples::get_peak_average_serial_optimized(48, vessel, 100, "DA");
         // This finds peak average of H - H does not exist in all examples
     }
-    debug_print("End circadian_rhythm_single_core_10_runs_optimized");
+    debug_print("End circadian_rhythm_single_core_100_runs_optimized");
 }
 
-BENCHMARK(circadian_rhythm_single_core_10_runs_optimized)->Unit(benchmark::kMillisecond);
+BENCHMARK(circadian_rhythm_single_core_100_runs_optimized)->Unit(benchmark::kMillisecond)->Iterations(50);
 
 
 static void circadian_rhythm_multi_core_100_runs(benchmark::State &state) {
@@ -71,12 +71,12 @@ static void circadian_rhythm_multi_core_100_runs(benchmark::State &state) {
 
     debug_print("\nStarting circadian_rhythm_multi_core_100_runs");
     for (const auto _: state) {
-        Examples::get_peak_average(1500, vessel, 100, "DA");
+        Examples::get_peak_average(48, vessel, 100, "DA");
     }
     debug_print("End circadian_rhythm_multi_core_100_runs");
 }
 
-BENCHMARK(circadian_rhythm_multi_core_100_runs)->Unit(benchmark::kMillisecond);
+BENCHMARK(circadian_rhythm_multi_core_100_runs)->Unit(benchmark::kMillisecond)->Iterations(50);
 
 
 static void exponential_decay_single_core(benchmark::State &state) {
@@ -84,12 +84,12 @@ static void exponential_decay_single_core(benchmark::State &state) {
 
     debug_print("\nStarting exponential_decay_single_core");
     for (const auto _: state) {
-        Examples::get_peak_average_serial(1500, vessel, 100, "C");
+        Examples::get_peak_average_serial(48, vessel, 100, "C");
     }
     debug_print("End exponential_decay_single_core");
 }
 
-BENCHMARK(exponential_decay_single_core)->Unit(benchmark::kMillisecond);
+BENCHMARK(exponential_decay_single_core)->Unit(benchmark::kMillisecond)->Iterations(50);
 
 
 static void exponential_decay_multi_core(benchmark::State &state) {
@@ -102,6 +102,6 @@ static void exponential_decay_multi_core(benchmark::State &state) {
     debug_print("End exponential_decay_multi_core");
 }
 
-BENCHMARK(exponential_decay_multi_core)->Unit(benchmark::kMillisecond);
+BENCHMARK(exponential_decay_multi_core)->Unit(benchmark::kMillisecond)->Iterations(50);
 
 BENCHMARK_MAIN();
